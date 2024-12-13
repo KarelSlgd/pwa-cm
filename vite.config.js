ae8813a353466deb1c99edb6b7c1d972f3e22db7
KarelSlgd <20213tn076@utez.edu.mx>
2024-12-13 09:50:45 -0600

    m

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig(({ mode }) => {
               purpose: "any",
             },
           ],
-          start_url: "/pwa-cm/",
+          start_url: "/pwa-cm/admin225ij5a2o1uzptgeo9g",
           display: "standalone",
           background_color: "#ffffff",
           theme_color: "#000000",
